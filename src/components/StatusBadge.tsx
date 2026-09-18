20954a37b3071197162cf1f322320a1c2368eba5
CookMonster123 <143905733+CookMonster123@users.noreply.github.com>
2026-09-18 06:17:08 -0400

    Delete src directory

--- src/components/StatusBadge.tsx
+++
@@ -1,4 +0,0 @@
-import type { BuildStatus } from '../types/core';
-export default function StatusBadge({status}:{status:BuildStatus}) {
-  return <span className={`status-badge status-${status.toLowerCase().replaceAll('/','-').replaceAll(' ','-')}`}>{status}</span>;
-}
